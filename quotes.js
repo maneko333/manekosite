181e56369a53370a8863a6a622c56138a374c4a0
maneko <maneko333@protonmail.com>
2025-03-05 21:20:23 +0000

    Web edit

--- quotes.js
+++ quotes.js
@@ -48,7 +48,48 @@ var lyricsStore = [
 		lyric: "You will always be the heart in me. You will always be the past, the love, the memories.",
 		title: "The Past the Love the Memory",
 		artist: "A Skylit Drive"
+	},
+	{
+		lyric: "Look at the sky, I'm still here. I'll be alive next year. I can make something good.",
+		title: "Look at the Sky",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "If you saw me, a stranger in the street, could you tell me you love me? Maybe we should both make real friends.",
+		title: "Perfect Pinterest Garden",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "Am I just one more face through the door? What's it all for? I'm taking you home.",
+		title: "Mona Lisa",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "Saw me fondly. You don't know me... but you know me.",
+		title: "Everything to Me",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "It's you and me and the air in-between. All of me and all of you. Nothing else in the room. This seems like it's everything to me.",
+		title: "Everything to Me",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "I spewed out that irresponsible mantra, 'I'm sorry I'm like this'.",
+		title: "Year of the Cup",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "It's not fair. 'Cause I knew you like the back of my hands. Well, don't you care? I gave you everything. Now I feel you even when you're not there.",
+		title: "Cheerleader",
+		artist: "Porter Robinson"
+	},
+	{
+		lyric: "It's kinda sad how it's not your fault you're living in a madhouse.",
+		title: "Cheerleader",
+		artist: "Porter Robinson"
 	}
+
 ];
 
 nextButton.addEventListener('click', randLyric);
