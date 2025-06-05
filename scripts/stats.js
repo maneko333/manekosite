8f4b4f542596278f6ebb83e3799f5e12b040e1b8
maneko333 <170786578+maneko333@users.noreply.github.com>
2025-06-04 23:03:12 -0400

    start work on stats

--- scripts/stats.js
+++ scripts/stats.js
@@ -10,10 +10,10 @@ let username = "maneko"; // <<<--- Insert your username here!
     const updated = new Date(json.updated_at).toLocaleDateString(); // Formats Last Updated text
     const created = new Date(json.created_at).toLocaleDateString(); // Formats Creation Date text
 
-    if (document.getElementById("created")) document.getElementById("created").innerHTML = `<em>Created</em>: ${created}`;
-    if (document.getElementById("updated")) document.getElementById("updated").innerHTML = `<em>Updated</em>: ${updated}`;
-    if (document.getElementById("visitors")) document.getElementById("visitors").innerHTML = `<em>Visits</em>: ${json.views}`;
-    if (document.getElementById("followers")) document.getElementById("followers").innerHTML = `<em>Followers</em>: ${json.followers}`;
+    if (document.getElementById("created")) document.getElementById("created").innerHTML = `<span id="emphasis">Created</span>: ${created}`;
+    if (document.getElementById("updated")) document.getElementById("updated").innerHTML = `<span id="emphasis">Updated</span>: ${updated}`;
+    if (document.getElementById("visitors")) document.getElementById("visitors").innerHTML = `<span id="emphasis">Visits</span>: ${json.views}`;
+    if (document.getElementById("followers")) document.getElementById("followers").innerHTML = `<span id="emphasis">Followers</span>: ${json.followers}`;
   } catch (error) {
     console.error(error);
     // If you wish to insert some fallback here, you may do so!
